32a14fca25ff0a524edb8d5f7b8d0fc36d3955e1
Emir Vranac <vranacemir@gmail.com>
2022-07-04 22:48:15 +0200

    add some basic files and code inside just check configuration

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,8 @@
+import { Utils } from "../app/Utils";
+
+describe("Utils test suite", () => {
+  test("first test", () => {
+    const test = new Utils();
+    console.log("Testing setup => IT WORKS!!!");
+  });
+});
