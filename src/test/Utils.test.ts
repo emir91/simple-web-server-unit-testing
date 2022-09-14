b5de33dbc7d684ffce75eb6308d8c84f07240096
Emir Vranac <vranacemir@gmail.com>
2022-09-14 14:17:20 +0200

    Assertions: toBe and toEqual

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,30 @@
 import { Utils } from "../app/Utils";
 
 describe("Utils test suite", () => {
-  test("first test", () => {
-    const test = new Utils();
-    console.log("Testing setup => IT WORKS!!!");
+  test("Upper Case method test", () => {
+    const toUpperCase = Utils.toUpperCase("abc");
+    const resultStr = "ABC";
+
+    expect(toUpperCase).toBe(resultStr);
+  });
+
+  test("Parse Url method test - url without query string", () => {
+    const parseUrl = Utils.parseUrl("http://localhost:8080/login");
+
+    expect(parseUrl.protocol).toBe("http:");
+    expect(parseUrl.host).toBe("localhost:8080");
+    expect(parseUrl.pathname).toBe("/login");
+  });
+
+  test("Parse Url method test - url with query string", () => {
+    const parseUrl = Utils.parseUrl(
+      "http://localhost:8080/login?username=user&password=pass"
+    );
+    const result = {
+      username: "user",
+      password: "pass",
+    };
+
+    expect(parseUrl.query).toEqual(result);
   });
 });
